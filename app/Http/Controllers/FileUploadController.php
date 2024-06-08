5b2dfc7ebb148cf6aaba8cad350e9a2ab629efa9
JoyoS58 <joyosugito99@gmail.com>
2024-06-08 20:50:06 +0700

    PWL - Jobsheet 12 E. MEMBUAT SYMLINK F. METHOD MOVE

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -31,18 +31,38 @@ class FileUploadController extends Controller
         //     echo "Tidak ada berkas yang diupload";
         // }
 
-        $request->validate([
-            'berkas'=>'required|file|image|max:500',
-        ]);
-        $extfile=$request->berkas->getClientOriginalName();
-        $namaFile='web-'.time().".".$extfile;
-        $path = $request->berkas->stoerAs('uploads',$namaFile);
-        echo "proses upload berhasil, data disimpan pada: $path";
+        // $request->validate([
+        //     'berkas'=>'required|file|image|max:500',
+        // ]);
+        // $extfile=$request->berkas->getClientOriginalName();
+        // $namaFile='web-'.time().".".$extfile;
+        // $path = $request->berkas->stoerAs('public',$namaFile);
+
+        // $pathBaru=asset('storage/'.$namaFile);
+        // // $path = $request->berkas->stoerAs('uploads',$namaFile);
+        // echo "proses upload berhasil, data disimpan pada: $path";
         // $namaFile=$request->berkas->getClientOriginalName();
         // $path = $request->berkas->storeAs('uploads',$namaFile);
         // $path = $request->berkas->storeAs('uploads','berkas');
         // $path = $request->berkas->store('uploads'); //store digunakan untuk menggenerate nama acak untuk file upload 
         // echo "proses upload berhasil, file berada di: $path";
         // echo $request->berkas->getClientOriginalName()."lolos validasi";
+        // echo "<br>";
+        // echo "Tampilkan link: <a href='$pathBaru'>$pathBaru</a>";
+
+        $request->validate([
+            'berkas'=>'required|file|image|max:500',
+        ]);
+        $extfile=$request->berkas->getClientOriginalName();
+        $namaFile='web-'.time().".".$extfile;
+
+        $path = $request->berkas->move('gambar',$namaFile);
+        $path = str_replace("\\","//",$path);
+        echo "Variabel path berisi:$path <br>";
+
+        $pathBaru = asset('gambar/'.$namaFile);
+        echo "proses upload berhasil, data di simpan pada:$path";
+        echo "<br>";
+        echo "Tampilakan link:<a href='$pathBaru'>$pathBaru</a>";
     }
 }
